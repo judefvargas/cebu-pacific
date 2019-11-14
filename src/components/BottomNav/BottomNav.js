ac91505020bcea06da4113ade793d3df7a32d65a
Jude <judefvargas@gmail.com>
2019-11-14 22:17:42 +0800

    Properly button alignment

--- src/components/BottomNav/BottomNav.js
+++ src/components/BottomNav/BottomNav.js
@@ -17,7 +17,7 @@ export default class BottomNav extends Component {
                         <div className="col-md-8">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                         </div>
-                        <div className="col-md-4">
+                        <div className="col-md-4 alignLeft">
                             <Button onClick={this.btnClick.bind(this, 'yes')} bsPrefix="btnnav btn" variant="success" size="lg">YES</Button>
                             <Button onClick={this.btnClick.bind(this, 'no')} bsPrefix="btnnav btn" variant="danger" size="lg">NO</Button>
                         </div>
