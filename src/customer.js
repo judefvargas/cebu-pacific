e1b730a9bb34afd8e6aa8b11be990a6d6e57192f
Jude <judefvargas@gmail.com>
2019-11-21 11:00:19 +0800

    Updated customer list and conversation

--- src/customer.js
+++ src/customer.js
@@ -27,102 +27,98 @@ export const CUSTOMERS = [
         image: '3.png',
         text: "I'd like to transfer my dog"
       },
-      {
-        id: 1, 
-        name: "D Vargas",
-        age: 25,
-        image: '4.png',
-        text: "I'd like to transfer my dog"
-      },
-      {
-        id: 1, 
-        name: "E Vargas",
-        age: 25,
-        image: '5.png',
-        text: "I'd like to transfer my dog"
-      },
-      {
-        id: 1, 
-        name: "F Vargas",
-        age: 25,
-        image: '12.png',
-        text: "I'd like to transfer my dog"
-      },
-      {
-        id: 1, 
-        name: "G Vargas",
-        age: 25,
-        image: '7.png',
-        text: "I'd like to transfer my dog"
-      },
-      {
-        id: 1, 
-        name: "H Vargas",
-        age: 25,
-        image: '8.png',
-        text: "I'd like to transfer my dog"
-      },
-      {
-        id: 1, 
-        name: "I Vargas",
-        age: 25,
-        image: '9.png',
-        text: "I'd like to transfer my dog"
-      },
-      {
-        id: 1, 
-        name: "J Vargas",
-        age: 25,
-        image: '10.png',
-        text: "I'd like to transfer my dog"
-      },
-      {
-        id: 1, 
-        name: "K Vargas",
-        age: 25,
-        image: 'customer1.png',
-        text: "I'd like to transfer my dog"
-      },
-      {
-        id: 1, 
-        name: "L Vargas",
-        age: 25,
-        image: 'customer1.png',
-        text: "I'd like to transfer my dog"
-      },
-      {
-        id: 1, 
-        name: "M Vargas",
-        age: 25,
-        image: 'customer1.png',
-        text: "I'd like to transfer my dog"
-      },
+      // {
+      //   id: 1, 
+      //   name: "D Vargas",
+      //   age: 25,
+      //   image: '4.png',
+      //   text: "I'd like to transfer my dog"
+      // },
+      // {
+      //   id: 1, 
+      //   name: "E Vargas",
+      //   age: 25,
+      //   image: '5.png',
+      //   text: "I'd like to transfer my dog"
+      // },
+      // {
+      //   id: 1, 
+      //   name: "F Vargas",
+      //   age: 25,
+      //   image: '12.png',
+      //   text: "I'd like to transfer my dog"
+      // },
+      // {
+      //   id: 1, 
+      //   name: "G Vargas",
+      //   age: 25,
+      //   image: '7.png',
+      //   text: "I'd like to transfer my dog"
+      // },
+      // {
+      //   id: 1, 
+      //   name: "H Vargas",
+      //   age: 25,
+      //   image: '8.png',
+      //   text: "I'd like to transfer my dog"
+      // },
+      // {
+      //   id: 1, 
+      //   name: "I Vargas",
+      //   age: 25,
+      //   image: '9.png',
+      //   text: "I'd like to transfer my dog"
+      // },
+      // {
+      //   id: 1, 
+      //   name: "J Vargas",
+      //   age: 25,
+      //   image: '10.png',
+      //   text: "I'd like to transfer my dog"
+      // },
+      // {
+      //   id: 1, 
+      //   name: "K Vargas",
+      //   age: 25,
+      //   image: 'customer1.png',
+      //   text: "I'd like to transfer my dog"
+      // },
+      // {
+      //   id: 1, 
+      //   name: "L Vargas",
+      //   age: 25,
+      //   image: 'customer1.png',
+      //   text: "I'd like to transfer my dog"
+      // },
+      // {
+      //   id: 1, 
+      //   name: "M Vargas",
+      //   age: 25,
+      //   image: 'customer1.png',
+      //   text: "I'd like to transfer my dog"
+      // },
 ];
 
 export const CONVERSATION = [
   {
-    customer: 'Duis aute irure dolor in reprehenderit in ',
-    john: 'dolor sit amet, consectetur adipiscing elit'
+    john: 'Hello po, I’m John. Happy to serve you. Ano pong maipaglilingkod namin sa inyo?',
+    customer: 'English please.',
   },
   {
-    customer: 'Ut enim ad minim veniam, quis nostrud',
-    john: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    john: 'Oh, how may I help you sir?',
+    customer: 'I want to change my Dollars into Pesos',
   },
   {
-    customer: 'Duis aute irure dolor in reprehenderit in ',
-    john: 'dolor sit amet, consectetur adipiscing elit'
+    john: 'No problem, sir. Please fill out this form.',
+    customer: 'Excuse me, is the purpose of purchase really necessary? What I do with my money is none of your business. This is an invasion of privacy!',
   },
-  {
-    customer: 'Ut enim ad minim veniam, quis nostrud',
-    john: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-  },
-  {
-    customer: 'Duis aute irure dolor in reprehenderit in ',
-    john: 'dolor sit amet, consectetur adipiscing elit'
-  },
-  {
-    customer: 'Ut enim ad minim veniam, quis nostrud',
-    john: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-  },
+  // {
+  //   customer: 'Duis aute irure dolor in reprehenderit in ',
+  //   john: 'dolor sit amet, consectetur adipiscing elit'
+  // },
+  // {
+  //   customer: 'Ut enim ad minim veniam, quis nostrud',
+  //   john: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+  // },
  
 ];
