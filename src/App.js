e96475dd0eecbc98a0c37a191063cd93113e7885
Jude <judefvargas@gmail.com>
2019-11-21 11:05:32 +0800

    Font awesome icon (in distractors)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import Main from './Main';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCaretLeft, faCaretRight, faInfo, faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCaretLeft, faCaretRight, faInfo, faCircle, faCalendar, faLayerGroup } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   document.body.style.overflow = "hidden";
-  library.add(faCaretLeft, faCaretRight, faInfo, faCircle);
+  library.add(faCaretLeft, faCaretRight, faInfo, faCircle, faCalendar, faLayerGroup);
   return (
     <div className="App">
       <Main />      
