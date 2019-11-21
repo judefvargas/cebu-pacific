b1782a6848e5454e7b390b5e9c13f6852046e934
Jude <judefvargas@gmail.com>
2019-11-21 11:03:21 +0800

    Added Clock component and date in right panel (main nav)

---
+++ src/components/MainNav/Clock.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Clock extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            date: new Date(2019, 10, 14, 10, 40)
+            // date: new Date(2019, 10, 14, 10, 40, 20),
+        }
+        setInterval(
+            () => this.setState({date: new Date(this.state.date.setMinutes(this.state.date.getMinutes()+10) ) }),
+            5000
+        ); //tick every 5 seconds, add 10 minutes to date
+    }
+    
+    render() {
+        return (
+            <FormatDate date={this.state.date} />
+        )
+    }
+}
+
+const FormatDate = (props) => {
+    let { date } = props;
+    let actual = date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+    return (
+        <>
+            { actual }
+        </>
+    )
+}
--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
 import './mainnav.css';
-import Clock from 'react-clock'
+// import Clock from 'react-clock/'
+import Clock from './Clock';
 import Interaction from './Interaction';
-import Button from 'react-bootstrap/Button';
+import Webobject from './Webobject';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class MainNav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            size: 80
-        }
-    }
     btnClick = (pos) => {
         alert(`${pos} clicked`);
     }
@@ -18,11 +14,17 @@ export default class MainNav extends Component {
         return (
             <div className="row grid-main-nav">
                 <Interaction/>
-                <WebObject/>
+                <Webobject/>
 
                 <div className="col col-md-1 distractors">
-                    <div className="clock"> <Clock size={this.state.size} value={new Date()}/></div>
-                    <div><CurrentDate/></div>
+                    <div className="timer clock"> <Clock /></div>
+                    {/* timezone={'Asia/Manila'} */}
+                    {/*  */}
+                    <span >
+                    <div className="class2"><CurrentDate/></div>
+                        <FontAwesomeIcon inverse size="5x" icon="calendar" />
+                    </span>
+                    {/* <Calendar/> */}
                     <div onClick={this.btnClick.bind(this, 'job aids')} className="calloutRight">JOB AIDS</div>
                 </div>
             </div>
@@ -31,30 +33,15 @@ export default class MainNav extends Component {
 }
 
 const CurrentDate = () => {
+    let monthNames = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JULY', 'AUG', 'SEPT', 'OCT', 'NOV', 'DEC']
     let currentDate = new Date();
-    let month = currentDate.getMonth();
+    let month = monthNames[currentDate.getMonth()];
     let date = currentDate.getDate();
-    let year = currentDate.getFullYear();
+    // let year = currentDate.getFullYear();
 
-    let actualDate = `${month}/${date}/${year}`;
+    let actualDate = `${month}. ${date}`;
     return (
         <>{actualDate}</>
     );
 }
 
-const WebObject = () => {
-    const btnClick = (pos) => {
-        alert(`${pos} clicked`);
-    }
-    return (
-        <div className="col col-md-9 weboject">
-            <div className="mainNavBtns">
-                <Button onClick={btnClick.bind(this, 'web object button')} bsPrefix="btn btn-info btn-lg jBtn1" size="lg" variant="info">Jewelry</Button>
-                <Button onClick={btnClick.bind(this, 'web object button')} bsPrefix="btn btn-info btn-lg jBtn1" size="lg" variant="info">T</Button>
-                <Button onClick={btnClick.bind(this, 'web object button')} bsPrefix="btn btn-info btn-lg jBtn1" size="lg" variant="info">I</Button>
-                <Button onClick={btnClick.bind(this, 'web object button')} bsPrefix="btn btn-info btn-lg jBtn1" size="lg" variant="info">L</Button>
-                <Button onClick={btnClick.bind(this, 'web object button')} bsPrefix="btn btn-info btn-lg jBtn1" size="lg" variant="info">L</Button>
-            </div>
-        </div>
-    );
-}
