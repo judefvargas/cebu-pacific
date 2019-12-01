e141ca560cd807504796a39c094c8df6e80afa92
Jude <judefvargas@gmail.com>
2019-12-01 23:13:49 +0800

    Added setter for selected/active customer. Placed components inside Error Handler

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 import TopNav from './components/TopNav/TopNav';
 import MainNav from './components/MainNav/MainNav';
+import { CUSTOMERS, currentCustomer } from './customer';
+import ErrorHandler from './components/ErrorHandler';
 
 export default class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentCustomer: currentCustomer,
+            activeCustomer: CUSTOMERS[currentCustomer], //pull from storyline variable currentCustomer* with initial 0
+            totalCount: CUSTOMERS.length
+        }
+    }
+    incrementActive = () => {
+        this.setState({ 
+            currentCustomer: this.state.currentCustomer+1,
+            activeCustomer: CUSTOMERS[this.state.currentCustomer+1]
+        });
+    }
     render() {
         return (
             <>
-                <TopNav />
-                <MainNav />
+                <ErrorHandler>
+                <TopNav next={ this.incrementActive } total={this.state.totalCount} active={this.state.activeCustomer} />
+                <MainNav next={ this.incrementActive } active={this.state.activeCustomer} />
+                </ErrorHandler>
             </>
         )
     }
