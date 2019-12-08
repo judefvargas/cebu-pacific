e072e585ac8bc6bf44319c08bc797fe2c4555532
Jude <judefvargas@gmail.com>
2019-12-08 23:23:56 +0800

    Show modal (SL), and proper searching of active customer

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import TopNav from './components/TopNav/TopNav';
 import MainNav from './components/MainNav/MainNav';
-import { CUSTOMERS, currentCustomer } from './customer';
+import { CUSTOMERS, currentCustomer, isShowModal } from './customer';
 import ErrorHandler from './components/ErrorHandler';
 
 export default class Main extends Component {
@@ -10,15 +10,21 @@ export default class Main extends Component {
         this.state = {
             currentCustomer: currentCustomer,
             activeCustomer: this.search(currentCustomer, CUSTOMERS), //pull from storyline variable currentCustomer* with initial 0
-            totalCount: CUSTOMERS.length
+            totalCount: CUSTOMERS.length,
+            isShowModal: isShowModal
         }
     }
-
     incrementActive = () => {
-        this.setState({ 
-            currentCustomer: this.state.currentCustomer+1,
-            activeCustomer: this.search(currentCustomer, CUSTOMERS)
-        });
+        const { currentCustomer, totalCount } = this.state;
+        if ((currentCustomer+1) > totalCount) {
+            this.setState({isShowModal: true});
+            console.log('show modal here');
+        } else {
+            this.setState({ 
+                currentCustomer: currentCustomer+1,
+                activeCustomer: this.search(this.state.currentCustomer+1, CUSTOMERS)
+            });
+        }
     }
 
     search = (value, array) => {
