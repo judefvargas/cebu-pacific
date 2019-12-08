3fbbb7e0ac66b67b1ebd3d35baa0ed69018bb1f7
Jude <judefvargas@gmail.com>
2019-12-09 00:16:46 +0800

    Updated conflicts

--- src/components/MainNav/Interaction.js
+++ src/components/MainNav/Interaction.js
@@ -9,7 +9,6 @@ import { styles } from '../animationStyles';
 export default function Interaction(props) {
     const messageEndRef = useRef(null);
     const current = [];
-    // const [isOn, turnOn] = useState(false);
     Object.keys(convoR).forEach(function(key) {
         if (parseInt(key) === props.active.id) current.push(convoR[key]);
     });
