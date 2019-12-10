75c35da40bb22b4eba339c77de667ed2566147fc
Jude <judefvargas@gmail.com>
2019-12-10 22:26:26 +0800

    Updated top nav interface

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -14,15 +14,16 @@ export default class TopNav extends Component {
           <div className="row grid-top-nav">
             <div className="col-md-4" style={{ paddingRight: 0 }}>
               <div className="col-md-12 interaction">
+                <img src="PPS logo.png" alt="" style={{width:'100px', float: 'left'}}/>
               <OverlayTrigger trigger="hover" placement="right" overlay={popover}>
-                  <span className="fa-layers fa-fw">
+                  <span className="fa-layers fa-fw information">
                       <FontAwesomeIcon icon="circle" size="lg" color="red" />
                       <FontAwesomeIcon icon="info" inverse size="sm"/>
                   </span>
               </OverlayTrigger>
               <div className="currentCount">{this.props.active.id}/{this.props.total}</div>
               <img className="customerInteraction" height="100%" width="76px" alt="" src={`characters/${this.props.active.image}`} />
-              <img className="john" height="100px" width="100px" alt="" src={`characters/john.png`} />
+              {/* <img className="john" height="100px" width="100px" alt="" src={`characters/john.png`} /> */}
               </div>
             </div>
             <div className="col-md-8 col-top-nav" >
