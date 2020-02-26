f617009c8f4dce0e1deb872bb6c59845f7e2772e
Jude <judefvargas@gmail.com>
2020-02-26 08:47:51 +0800

    updated customer.js/updated wrong branch

--- src/customer.js
+++ src/customer.js
@@ -1,8 +1,8 @@
 export const player = window.parent.GetPlayer();
 
-export const CUSTOMERS = [    {       "id":1,      "name":"Annabelle Reyes",      "age":25,      "image":"4.png",      "package":"bag.png",      "text":"I'd like to transfer my dog", "time": "10:40", "date":"January 12"    }, {       "id":2,      "name":"Robert Downey",      "age":32,      "image":"1.png",      "package":"money.png",      "text":"I'd like to transfer my bag", "time": "1:40", "date":"October 12"   }, {       "id":3,      "name":"Daniela Reyes",      "age":12,      "image":"5.png",      "package":"money.png",      "text":"I'd like to transfer my bag", "time": "1:40", "date":"October 12"   }];
+export const CUSTOMERS = JSON.parse(player.GetVar('CHAT_customers'));
 
-export const convoR = ({    "1":[       {          "customer":"Ipapapalit ko sana to, kaso hindi ko na alam kung anu-anong mga pera ‘to kasi natira lang ‘to sa biyahe namin."      },    "q1", "q2" ], "2":[       {          "customer":"Magandang Umaga po."      },    {          "customer":"Magpapapalit po sana ako ng pera."      }, {          "john":"Ah sige po, magkano po ang ipapapalit ninyo"      }, "q2" ], "3":[       {          "customer":"Sample sample sadsadsadkljasldjljl."      },    "q1", "q2" ]});
+export const convoR = JSON.parse(player.GetVar('CHAT_conversation'));
 
 export const questionList = JSON.parse(player.GetVar('CHAT_questionList'));
 
@@ -14,7 +14,7 @@ export const currentCustomer = player.GetVar('CHAT_curCustomer');
 
 export const consequences = JSON.parse(player.GetVar('CHAT_consequences'));
 
-export const buttons =  ([    {       "id":1,      "title":"Mag TILL ng Pera"   }]);
+export const buttons =  JSON.parse(player.GetVar('CHAT_buttons'));
 
 export const isShowModal = player.GetVar('CHAT_showModal');
 
@@ -32,12 +32,8 @@ export const allowClick = player.GetVar('CHAT_allowClick');
 
 export const scoring = JSON.parse(player.GetVar('CHAT_scoring'));
 
-export const buttonTitles = {
-    "job_aids": "Job Aids", 
-    "tutorial": "Tutorial"
-}
+export const buttonTitles = JSON.parse(player.GetVar('CHAT_buttonTitles'));
 
-export const pastChoices = {};
+export const pastChoices = JSON.parse(player.GetVar('CHAT_pastChoices'));
 
-export const allowImgClick = true;
-
+export const allowImgClick = player.GetVar('CHAT_allowTillmageClick');
